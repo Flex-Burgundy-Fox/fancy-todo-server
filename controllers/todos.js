ffedba3234eab2e260cecb83f93b40dd8b555d37
tohajasper <tohajasper@gmail.com>
2021-07-21 16:46:43 +0700

    CRUD Done

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,7 +1,105 @@
-const {Todos} = require('../models')
+const {Todo} = require('../models')
 
 class Controller {
-    
+
+    static addTodo (req, res){
+        let input = {
+            title : req.body.title,
+            description : req.body.description,
+            status : req.body.status,
+            due_date : req.body.due_date
+        }
+        Todo.create(input)
+        .then((result) => {
+            res.status(201).json(result)
+        }).catch((err) => {
+            if(err.name === "SequelizeValidationError") res.status(400).json(err)
+            else res.status(500).json(err)
+        });
+    }
+
+    static viewTodo (req, res){
+        Todo.findAll()
+        .then((result) => {
+            res.status(200).json(result)
+        }).catch((err) => {
+            res.status(500).json(err)
+        });
+    }
+
+    static findTodo (req, res){
+        Todo.findAll(req.body, {
+            where : {
+                id : +req.params.id
+            }
+        })
+        .then((result) => {
+            if(!result.length) throw {message : "error not found"}
+            res.status(200).json(result[0])
+        }).catch((err) => {
+            res.status(404).json(err)
+        });
+    }
+
+    static putTodo (req, res){
+        Todo.update(req.body,{
+            where : {
+                id : +req.params.id
+            },
+            returning : true
+        })
+        .then((result) => {
+            // console.log(result);
+            if(!result[0]) throw {message : "error not found"}
+            else res.status(200).json(result[1][0])
+        }).catch((err) => {
+            if(err.name === "SequelizeValidationError") res.status(400).json(err)
+            if(err.message === "error not found")res.status(404).json(err)
+            else res.status(500).json(err)
+        });
+    }
+
+    static patchTodo (req, res){
+        let input = { status : req.body.status}
+        Todo.update(input,{
+            where : {
+                id : +req.params.id
+            },
+            returning : true
+        })
+        .then((result) => {
+            // console.log(result);
+            if(!result[0]) throw {message : "error not found"}
+            else res.status(200).json(result[1][0])
+        }).catch((err) => {
+            if(err.name === "SequelizeValidationError") res.status(400).json(err)
+            if(err.message === "error not found")res.status(404).json(err)
+            else res.status(500).json(err)
+        });
+    }
+
+    static deleteTodo (req, res){
+        Todo.findByPk(+req.params.id)
+        .then((todo) => {
+            if(!todo) throw {message : "error not found"}
+
+            Todo.destroy({
+                where : {
+                    id : +req.params.id
+                }
+            })
+            .then((result) => {
+                res.status(200).json({message : 'todo success to delete', deletedData : todo})
+            }).catch((err) => {
+                throw err
+            });
+            
+        }).catch((err) => {
+            if(err.message === "error not found")res.status(404).json(err)
+            else res.status(500).json(err)
+        });
+    }
+
 }
 
 module.exports = Controller
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,12 @@ const Controller = require('../controllers/todos')
 
 const router = require('express').Router()
 
-router.post("/todos",Controller)
+router.post("/todos",Controller.addTodo)
+router.get("/todos",Controller.viewTodo)
+router.get("/todos/:id",Controller.findTodo)
+router.put("/todos/:id",Controller.putTodo)
+router.patch("/todos/:id",Controller.patchTodo)
+router.delete("/todos/:id",Controller.deleteTodo)
+
 
 module.exports = router
