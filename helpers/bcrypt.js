44b34b6acfaf1975da11dff6852b0adffa197e69
Panji Maulana <panjinmaulana@gmail.com>
2021-07-22 21:55:58 +0700

    Second commit

---
+++ helpers/bcrypt.js
@@ -0,0 +1,20 @@
+const bcrypt = require("bcrypt")
+const saltRounds = 10
+
+function hashingPassword(plaintextPassword ) {
+  const salt = bcrypt.genSaltSync(saltRounds)
+  const hash = bcrypt.hashSync(plaintextPassword, salt)
+  // Store hash in your password DB.
+
+  return hash
+}
+
+function comparePassword(plaintextPassword, hash) {
+  // Load hash from your password DB.
+  return bcrypt.compareSync(plaintextPassword, hash)
+}
+
+module.exports = { 
+  hashingPassword,
+  comparePassword
+ }
