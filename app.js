9c2eaa56e8ef6af21366616523dbd3802af838e7
Panji Maulana <panjinmaulana@gmail.com>
2021-07-27 21:49:24 +0700

    cors

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ require("dotenv").config()
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require("cors")
 
 const routes = require('./routes')
 
 const errorHandlers =  require("./middlewares/errorHandlers")
+
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
