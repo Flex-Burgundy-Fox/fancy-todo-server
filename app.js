27ffffe40de509ae780b6a1b80218d5c00826b5a
Andhika Megantara <81455956+megzgambino@users.noreply.github.com>
2021-07-29 17:31:16 +0700

    added cors into the dependencies

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = express()
 const PORT = 3000
 const errorHandler = require('./middlewares/ErrHandler')
 const router = require('./routes/index')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
