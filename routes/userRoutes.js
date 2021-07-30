87390b4f6f05f7615ba1faa6cf2c4ab7fa718f04
Panji Maulana <panjinmaulana@gmail.com>
2021-07-30 22:07:25 +0700

    google sign-in

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,7 @@ const userController = require("../controllers/userController")
 
 router.post("/register", userController.register)
 router.post("/login", userController.login)
+// google sign-in
+router.post("/google-login", userController.googleLogin)
 
 module.exports = router
