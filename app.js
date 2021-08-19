0f7d8af73c4c79590d02b5a3817b0ea085900081
Andhika Megantara <81455956+megzgambino@users.noreply.github.com>
2021-08-19 20:04:21 +0700

    deploy heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-require('dotenv').config()
+if (process.env.NODE_ENV != 'production') require('dotenv').config()
 const express = require('express')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const errorHandler = require('./middlewares/ErrHandler')
 const router = require('./routes/index')
 const cors = require('cors')
